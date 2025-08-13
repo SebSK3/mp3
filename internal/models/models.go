74770424b3c543de7d4fc7513fea100e8e334b63
SebSK3 <34949127+SebSK3@users.noreply.github.com>
2025-08-13 14:28:38 +0200

    Fixup model

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,15 +1,13 @@
 package internal
 
 type Song struct {
-	Id                  int64
-	Url                 string `form:"Url"`
-	Mix                 bool   `form:"Mix"`
-	Artist              string `form:"Artist,omitempty"`
-	Title               string `form:"Title,omitempty"`
-	Album               string `form:"Album,omitempty"`
-	CustomImageUrl      string `form:"ImgUrl,omitempty"`
-	CustomFFmpegCommand string `form:"FFmpegCmd,omitempty"`
-	Fullname            string
-	Filename            string
-	Progress            int64
+	Id       int64
+	Url      string `form:"Url"`
+	Mix      bool   `form:"Mix"`
+	Artist   string `form:"Artist,omitempty"`
+	Title    string `form:"Title,omitempty"`
+	Album    string `form:"Album,omitempty"`
+	Fullname string
+	Filename string
+	Progress int64
 }
